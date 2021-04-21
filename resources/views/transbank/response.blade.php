b505bffcb79a7c260614fad39eea77db4ed41cd8
rexygar <40875991+rexygar@users.noreply.github.com>
2021-04-21 16:11:10 -0400

    .

--- resources/views/transbank/response.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ $url }}" method="POST" id="form">
-    <input type="hidden" name="token_ws" id="token_ws" value="{{ $token }}">
-</form>
-
-<script>
-    document.getElementById('form').submit();
-</script>
