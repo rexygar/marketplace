884f714d5b54c598e64b3df37413ac807b2b9549
javo-desu <32529489+javo-desu@users.noreply.github.com>
2021-05-05 17:57:10 -0400

    update quienes somos

--- resources/views/componentes/quienes_hero3.blade.php
+++ resources/views/componentes/quienes_hero3.blade.php
@@ -5,5 +5,40 @@
 
 
 
+  <div class="col-span-12 grid gap-4 grid-cols-6 py-24 xs:px-6 sm:px-16 md:px-16 bg-white lg:px-16 2xl:px-16 xl:px-16 ">
+
+    <div class="col-span-1 py-4">
+      <a href="http://www.fundacionbanamor.cl/">
+        <img src="img/footer/ban-amor.png" alt="" srcset="">
+      </a>
+    </div>
+    <div class="col-span-1 py-4">
+      <a href="https://www.colbun.cl/">
+        <img src="img/footer/colbun.png" alt="" srcset="">
+      </a>
+    </div>
+    <div class="col-span-1 py-4">
+      <a href="https://marcaconsciente.cl/">
+        <img src="img/footer/marca-con.png" alt="" srcset="">
+      </a>
+    </div>
+    <div class="col-span-1 py-4">
+      <a href="https://www.quillota.cl/udel/">
+        <img src="img/footer/qui-dedeco.png" alt="" srcset="">
+      </a>
+    </div>
+    <div class="col-span-1 py-4">
+      <a href="https://quillota.cl/municipalidad/">
+        <img src="img/footer/quillota.png" alt="" srcset="">
+      </a>
+    </div>
+    <div class="col-span-1 py-4">
+      <a href="https://www.fundacionlasemilla.cl/">
+        <img src="img/footer/semilla.png" alt="" srcset="">
+      </a>
+    </div>
+
+
+  </div>
 
 </div>
