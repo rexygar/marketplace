52c85d0998dcef5c0138aa0520266675d50f141c
javo-desu <32529489+javo-desu@users.noreply.github.com>
2021-05-10 22:15:24 -0400

    fix searchbar

--- resources/views/componentes/Header.blade.php
+++ resources/views/componentes/Header.blade.php
@@ -1,8 +1,62 @@
+<style>
+    .bg-celeste {
+        background-color: #409bca !important;
+    }
+
+    @media screen and (min-width: 0px) and (max-width: 650px) {
+        .no_mostrar {
+            display: none !important;
+        }
+
+        /* show it on small screens */
+    }
+
+    /* show it on small screens */
+    }
+
+    @media screen and (min-width: 0px) and (max-width: 769px) {
+        .mostrar_xl {
+            display: block !important;
+        }
+
+        /* show it on small screens */
+    }
+
+    @media screen and (min-width: 360px) and (max-width: 769px) {
+        .mostrar {
+            display: block !important;
+        }
+
+        /* show it on small screens */
+    }
+
+    @media screen and (min-width: 0px) and (max-width: 769px) {
+        .no_mostrar_xl {
+            display: none !important;
+        }
+
+        /* show it on small screens */
+    }
  
+    @media screen and (min-width: 770px) {
+        .mostrar_x2 {
+            display: block !important;
+        }
+
+        /* show it on small screens */
+    }
+    @media screen and (min-width: 0px) and (max-width: 769px) {
+        .esconder {
+            display: none !important;
+        }
+
+        /* show it on small screens */
+    }
+</style>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-<div class="sm:block hidden xl:-mb-36 2xl:-mb-36 lg:-mb-24 md:-mb-20 sm:-mb-20" x-data="{ searchy : true}">
+<div class="no_mostrar_xl mostrar_x2" x-data="{ searchy : true}">
     <nav class="font-sans grid  grid-cols-12 text-justify content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 sm:items-baseline w-full"
-        style="background-color: transparent">
+        :class="{' bg-transparent': searchy, 'bg-celeste': !searchy}">
         <div class="grid grid-cols-12  col-span-12 md:grid-cols-12 lg:py-2">
             <div class="col-span-3 sm:col-span-3 md:col-span-3 lg:col-span-3 xl:col-span-3 2xl:col-span-3">
                 <div class="col-span-12">
@@ -12,64 +66,80 @@
                     </a>
                 </div>
             </div>
-            <div
-                class="md:col-span-9 lg:col-span-9 sm:col-span-9 xl:col-span-9 2xl:col-span-9 grid grid-cols-12 lg:ml-16 md:ml-6 sm:ml-6 xl::ml-24 2xl::ml-36">
+            <div class="md:col-span-9 lg:col-span-9 sm:col-span-9 xl:col-span-9 2xl:col-span-9 grid grid-cols-12">
 
                 <div class="col-span-12 grid grid-cols-12  ">
 
-                    <div class="col-span-12 xl:col-start-4 2xl:col-start-4 lg:col-start-2 md:col-start-2"><span
-                            class="font-bold text-white text-xl"><button type="button" @click="searchy = !searchy"
-                                class="text-white dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none sm:px-2 md:px-4 lg:px-10 xl:px-10 2xl:px-10 focus:text-gray-600 dark:focus:text-gray-400"
-                                aria-label="toggle menu">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </button></span>
-                        <span
-                            class="font-bold text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
-                                href="{{__('/') }}">INICIO
-                            </a></span>
-                        <span
-                            class="font-bold text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
-                                href="{{ __('/About') }}">QUIÉNES
-                                SOMOS</a></span>
-                        <span
-                            class="font-bold text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
-                                href="{{ __('/Categorias') }}">PRODUCTOS
-                            </a></span>
-                        <span
-                            class="font-bold text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
-                                href="{{__('/Blog') }}">BLOG</a></span></div>
+
+                    <div class="col-span-12  grid grid-cols-12">
+
+                        <div class="col-span-4">
+
+                            <form class="bg-white flex items-center rounded-full shadow-xl" action="{{ __('Search') }}"
+                                method="GET">
+
+
+                                <input id="searchfield" type="search" name="search"
+                                    class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+                                    placeholder="Buscar">
+
+                                <div class="p-4">
+                                    <button
+                                        class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+                                        <i class="fa fa-search" aria-hidden="true"></i>
+                                    </button>
+                                </div>
+
+                            </form>
+
+
+                        </div>
+                        <div class="col-span-8 py-4">
+                            <span
+                                class="font-bold  col-span-1 text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
+                                    href="/">INICIO
+                                </a></span>
+                            <span
+                                class="font-bold col-span-1 text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
+                                    href="/About">QUIÉNES
+                                    SOMOS</a></span>
+                            <span
+                                class="font-bold col-span-1 text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
+                                    href="/Categorias">PRODUCTOS
+                                </a></span>
+                            <span
+                                class="font-bold col-span-1 text-blue-900 text-xl sm:text-base md:text-xl lg:text-xl xl:text-xl 2xl:text-2xl lg:px-2 xl:px-2 2xl:px-2"><a
+                                    href="/Blog">BLOG</a></span>
+                        </div>
+
+                    </div>
+
 
                 </div>
-            </div>
 
+            </div>
         </div>
+
     </nav>
-    <div class="relative px-24 bg-white shadow-xl block" :class="{' hidden': searchy, 'block': !searchy}"
+</div>
+{{-- <div class="relative px-24 bg-white shadow-xl block" :class="{' hidden': searchy, 'block': !searchy}"
         id="search-content">
         <div class="container mx-auto py-4 text-black">
             <form action="{{ __('Search') }}" method="GET">
 
-                <input id="searchfield" type="search" name="search" placeholder="Search..." autofocus="autofocus"
-                    class="w-5/6 text-grey-800 transition focus:outline-none focus:border-transparent p-2 appearance-none leading-normal text-xl lg:text-2xl">
-                <button type="button" @click="searchy =  !searchy"
-                    class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none px-10 focus:text-gray-600 dark:focus:text-gray-400"
-                    aria-label="toggle menu">
-                    <i class="fas fa-times text-2xl"></i>
-                </button>
-            </form>
-        </div>
-    </div>
+<input id="searchfield" type="search" name="search" placeholder="Search..." autofocus="autofocus"
+    class="w-5/6 text-grey-800 transition focus:outline-none focus:border-transparent p-2 appearance-none leading-normal text-xl lg:text-2xl">
+<button type="button" @click="searchy =  !searchy"
+    class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none px-10 focus:text-gray-600 dark:focus:text-gray-400"
+    aria-label="toggle menu">
+    <i class="fas fa-times text-2xl"></i>
+</button>
+</form>
 </div>
-<style>
-    @media screen and (min-width: 0px) and (max-width: 650px) {
-        .no_mostrar {
-            display: none !important;
-        }
+</div> --}}
+</div>
 
-        /* show it on small screens */
-    }
-</style>
-<div class="block sm:hidden" x-data="{ searchy : true}">
+<div class="mostrar_xl " x-data="{ searchy : true}">
     <nav style="background-color: #44b0e8"
         class="font-sans grid grid-cols-12 text-justify content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-black shadow sm:items-baseline w-full">
         <div class="grid col-span-12  grid-cols-12 lg:py-2 ">
